b5eaca49cd7d2f087a7c69cc58e6e73c7f1023bb
Robin De Herdt <robindh95@gmail.com>
2017-05-01 19:40:27 +0200

    Added call to action section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,16 +6,40 @@
     <div class="header-search">
         <form class="form-inline">
             <input type="text" class="form-control search-input" name="search-term" placeholder="Search for a guitar">
-            <input type="submit" class="btn btn-default search-submit">
+            <input type="submit" class="btn btn-default search-submit" value="Search">
         </form>
     </div>
 </div>
 <div class="header-image" style="background-image: url('/images/electric-guitars.jpg');"></div>
 <div class="container">
     <div class="row row-padding-top">
-        <div class="col-md-12">
-            <h1>Hello world!</h1>
+        <h2>How does it work?</h2>
+    </div>
+    <div class="row">
+        <div class="col-md-4">
+            <div class="cta-item">
+                <i class="fa fa-music fa-4x" aria-hidden="true"></i>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae consectetur ex. Donec non sollicitudin erat. Aenean libero massa, lobortis eu consequat non, sollicitudin nec diam</p>
+                <a href="#">Read more</a>
+            </div>
         </div>
+        <div class="col-md-4">
+            <div class="cta-item">
+                <i class="fa fa-music fa-4x" aria-hidden="true"></i>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae consectetur ex. Donec non sollicitudin erat. Aenean libero massa, lobortis eu consequat non, sollicitudin nec diam</p>
+                <a href="#">Read more</a>
+            </div>
+        </div>
+        <div class="col-md-4 ">
+            <div class="cta-item">
+                <i class="fa fa-music fa-4x" aria-hidden="true"></i>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae consectetur ex. Donec non sollicitudin erat. Aenean libero massa, lobortis eu consequat non, sollicitudin nec diam</p>
+                <a href="#">Read more</a>
+            </div>
+        </div>
+    </div>
+    <div class="row row-padding-top">
+        <h2>Articles</h2>
     </div>
 </div>
 @endsection
