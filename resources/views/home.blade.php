d731ce8d8f272ee5e0f1e7b7465dbebe86aedb91
Robin De Herdt <robindh95@gmail.com>
2017-05-01 16:58:28 +0200

    Added search box

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="header-content">
+    <h1>Try my guitar</h1>
+    <div class="header-search">
+        <form class="form-inline">
+            <input type="text" class="form-control search-input" name="search-term" placeholder="Search for a guitar">
+            <input type="submit" class="btn btn-default search-submit">
+        </form>
+    </div>
+</div>
 <div class="header-image" style="background-image: url('/images/electric-guitars.jpg');"></div>
 <div class="container">
     <div class="row row-padding-top">
