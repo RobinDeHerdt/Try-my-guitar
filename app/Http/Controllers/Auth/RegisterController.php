f3f0b4d89633eaa2ce20bc420f4f7837d38f002b
Robin De Herdt <robindh95@gmail.com>
2017-05-04 11:42:21 +0200

    Add user role on register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -62,10 +62,16 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => bcrypt($data['password']),
-        ]);
+        $user = new User();
+
+        $user->name = $data['name'];
+        $user->email = $data['email'];
+        $user->password = bcrypt($data['password']);
+
+        $user->save();
+
+        $user->roles()->attach(3);
+
+        return $user;
     }
 }
