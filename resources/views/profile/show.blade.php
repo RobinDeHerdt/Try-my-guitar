b866dadec078637403189d1c844cf1b5ebf3243f
Robin De Herdt <robindh95@gmail.com>
2017-05-08 19:58:39 +0200

    Added chat buttons to profile

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -9,9 +9,20 @@
         <div class="header-image profile-header-image" style="background-image: url('/images/electric-guitars.jpg');"></div>
         <div class="container">
             <div class="row">
-                <div class="col-md-3 profile-image" style="background-image: url({{ Storage::disk('public')->url($user->image_uri) }})">
+                <div class="col-md-3 profile-image" style="background-image: url({{ Storage::disk('public')->url($user->image_uri) }})"></div>
+                <div class="col-md-3">
+                    <h1 class="profile-name">{{ $user->first_name . ' ' . $user->last_name }}</h1>
                 </div>
-                <h1 class="profile-name">{{ $user->first_name . ' ' . $user->last_name }}</h1>
+                <div class="col-md-2 col-md-offset-2">
+                    <div class="big-cta-button">
+                        <a href="#">Invite to chat</a>
+                    </div>
+                </div>
+                <div class="col-md-2">
+                    <div class="big-cta-button">
+                        <a href="#">Invite to group chat</a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
