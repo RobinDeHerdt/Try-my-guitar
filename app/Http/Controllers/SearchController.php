7454a3c4f2c956ddf655ece800a5215d3758974e
Robin De Herdt <robindh95@gmail.com>
2017-05-20 10:03:49 +0200

    Refactored search controller

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -13,6 +13,8 @@ class SearchController extends Controller
 {
     private $users;
     private $guitars;
+    private $filter_brands = [];
+    private $filter_types  = [];
 
     /**
      * Display the results page.
@@ -22,65 +24,16 @@ class SearchController extends Controller
     public function result(Request $request)
     {
         $input = strip_tags($request->term);
+
         // Check if the input is not empty.
         if (!empty($input) && !ctype_space($input)) {
-            // Split the string into terms and remove whitespace from both sides of the string.
-            $terms = preg_split('/\s+/', $input, -1, PREG_SPLIT_NO_EMPTY);
+            // If there is no querystring set for 'types' or 'brands',
+            // return an empty array, to avoid errors in the view.
+            $this->filter_types     = ($request->query('types') ? $request->query('types') : []);
+            $this->filter_brands    = ($request->query('brands') ? $request->query('brands') : []);
 
-            /**
-             * User search.
-             */
-            $most_relevant_user_results = collect();
-
-            if(count($terms) >= 2) {
-                $most_relevant_user_results = User::where('first_name', 'like', '%'.$terms[0].'%')->where('last_name', 'like', '%'.$terms[1].'%')->take(6)->get();
-            }
-
-            $less_relevant_user_results = User::where(function($q) use ($terms)
-            {
-                foreach ($terms as $term)
-                {
-                    $q->orWhere('first_name', 'like', '%'.$term.'%')
-                        ->orWhere('last_name', 'like', '%'.$term.'%');
-                }
-            })->take(6)->get();
-
-            $this->users = $most_relevant_user_results->merge($less_relevant_user_results);
-
-            /**
-             * Guitar search.
-             */
-            $query = Guitar::where(function($q) use ($terms) {
-                foreach ($terms as $term) {
-                    $q->orWhere('name', 'like', '%'.$term.'%');
-                }
-            });
-
-            // Filter types.
-            if($filter_types = $request->query('types')) {
-                foreach ($filter_types as $filter_type) {
-                    $query->whereHas('guitarTypes', function($q) use ($filter_type) {
-                       $q->where('id', $filter_type);
-                    });
-                }
-            } else {
-                $filter_types = [];
-            }
-
-            // Filter brands.
-            if($filter_brands = $request->query('brands')) {
-                foreach ($filter_brands as $filter_brand) {
-                    $query->whereHas('guitarBrand', function($q) use ($filter_brand) {
-                        $q->where('id', $filter_brand);
-                    });
-                }
-            } else {
-                $filter_brands = [];
-            }
-
-            // Execute the query. Get a maximum of 10 records.
-            $this->guitars = $query->paginate(10);
-
+            $this->userSearch($input);
+            $this->guitarSearch($input);
         } else {
             return back();
         }
@@ -94,9 +47,78 @@ class SearchController extends Controller
             'search_term'   => $input,
             'types'         => $types,
             'brands'        => $brands,
-            'filter_types'  => $filter_types,
-            'filter_brands' => $filter_brands,
+            'filter_types'  => $this->filter_types,
+            'filter_brands' => $this->filter_brands,
         ]);
+    }
+
+    /**
+     * User search query.
+     */
+    private function userSearch($input) {
+        // Split the string into terms and remove whitespace from both sides of the string.
+        $terms = preg_split('/\s+/', $input, -1, PREG_SPLIT_NO_EMPTY);
+
+        if(count($terms) >= 2) {
+            $most_relevant_results = User::where('first_name', 'like', '%'.$terms[0].'%')->where('last_name', 'like', '%'.$terms[1].'%')->take(6)->get();
+        } else {
+            $most_relevant_results = collect();
+        }
+
+        $less_relevant_results = User::where(function($q) use ($terms)
+        {
+            foreach ($terms as $term)
+            {
+                $q->orWhere('first_name', 'like', '%'.$term.'%')
+                    ->orWhere('last_name', 'like', '%'.$term.'%');
+            }
+        })->take(6)->get();
+
+        $this->users = $most_relevant_results->merge($less_relevant_results);
+    }
+
+    /**
+     * Guitar search query.
+     */
+    private function guitarSearch($input) {
+        // Split the string into terms and remove whitespace from both sides of the string.
+        $terms = preg_split('/\s+/', $input, -1, PREG_SPLIT_NO_EMPTY);
+
+        $most_relevant_query = Guitar::where('name', 'like', '%'.$input.'%');
+        
+        $less_relevant_query = Guitar::where(function($q) use ($terms) {
+            foreach ($terms as $term) {
+                $q->orWhere('name', 'like', '%'.$term.'%');
+            }
+        });
+
+        $most_relevant_results = $this->filterResults($most_relevant_query)->get();
+        $less_relevant_results = $this->filterResults($less_relevant_query)->get();
+
+        $this->guitars = $most_relevant_results->merge($less_relevant_results);
+    }
+
+    /**
+     * Filter the 'guitar search' query.
+     */
+    private function filterResults($query) {
+        if($this->filter_types) {
+            foreach ($this->filter_types as $filter_type) {
+                $query->whereHas('guitarTypes', function($q) use ($filter_type) {
+                    $q->where('id', $filter_type);
+                });
+            }
+        }
+
+        if($this->filter_brands) {
+            foreach ($this->filter_brands as $filter_brand) {
+                $query->whereHas('guitarBrand', function($q) use ($filter_brand) {
+                    $q->where('id', $filter_brand);
+                });
+            }
+        }
+
+        return $query;
     }
 
     /**
