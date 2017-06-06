f823cfb5a22c6caa73f4550289ff3bc0035feaab
Robin De Herdt <robindh95@gmail.com>
2017-06-06 11:51:57 +0200

    Layout fixes

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -8,18 +8,20 @@
     <div class="content {{ Auth::user() && Auth::user()->hasRole('administrator') ? 'admin-authenticated' : '' }}">
         <div class="header-image" style="background-image: url('/images/login-bg.jpg');"></div>
         <div class="container">
-            @if (Session::has('success-message'))
-                <div class="alert alert-success">{{ Session::get('success-message') }}</div>
-            @endif
-            @if (Session::has('info-message'))
-                <div class="alert alert-info">{{ Session::get('info-message') }}</div>
-            @endif
-            @if (Session::has('error-message'))
-                <div class="alert alert-danger">{{ Session::get('error-message') }}</div>
-            @endif
+            <div class="row">
+                @if (Session::has('success-message'))
+                    <div class="alert alert-success">{{ Session::get('success-message') }}</div>
+                @endif
+                @if (Session::has('info-message'))
+                    <div class="alert alert-info">{{ Session::get('info-message') }}</div>
+                @endif
+                @if (Session::has('error-message'))
+                    <div class="alert alert-danger">{{ Session::get('error-message') }}</div>
+                @endif
+            </div>
             @foreach($about_sections as $about_section)
-                    <h2>{{ $about_section->title }}</h2>
-                <div class="row col-container" id="{{ strtolower(kebab_case($about_section->title)) }}">
+                <h2 id="{{ strtolower(kebab_case($about_section->title)) }}">{{ $about_section->title }}</h2>
+                <div class="row col-container">
                     <div class="col-md-6">
                         <p>{{ $about_section->column_one }}</p>
                     </div>
@@ -28,28 +30,36 @@
                     </div>
                 </div>
             @endforeach
-            <div class="row col-container">
-                <h2>Contact</h2>
-                <form action="{{ route('contact') }}" method="POST">
-                    {{ csrf_field() }}
-                    <div class="col-md-8 col-md-offset-2">
-                        <div class="form-group">
-                            <label for="email">@lang('input.email') *</label>
-                            <input type="email" class="form-control" name="email" required>
+        </div>
+        <div class="contact-container" id="contact">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h2>Contact</h2>
+                    </div>
+                    <form action="{{ route('contact') }}" method="POST">
+                        {{ csrf_field() }}
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                <label for="email">@lang('input.email') *</label>
+                                <input type="email" class="form-control" name="email" placeholder="Your e-mail address" required>
+                            </div>
                         </div>
-                        <div class="form-group">
-                            <label for="message">@lang('input.your-message') *</label>
-                            <textarea name="message" cols="30" rows="10" class="form-control" required></textarea>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-4 col-md-offset-8">
-                                <div class="form-group">
-                                    <input type="submit" class="btn btn-primary form-control" value="Send">
+                        <div class="col-md-8">
+                            <div class="form-group">
+                                <label for="message">@lang('input.your-message') *</label>
+                                <textarea name="message" cols="30" rows="10" class="form-control" placeholder="Your message" required></textarea>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-4 col-md-offset-8">
+                                    <div class="form-group">
+                                        <input type="submit" class="btn btn-primary form-control" value="Send">
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </form>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -7,47 +7,45 @@
 @section('content')
     <div class="content">
         <div class="container">
-            <div class="row heading">
-                <h1>Control panel</h1>
-            </div>
-            <div class="row col-container">
-                <div class="col-md-12">
-                    @if($reports->isNotEmpty())
-                        <h3>Reports to be reviewed</h3>
-                        <hr>
-                        @foreach($reports as $report)
-                            <span>
-                                <a href="{{ route('profile.show', ['user' => $report->reporter->id]) }}">{{ $report->reporter->fullName() }}</a>
-                                has reported
-                                <a href="{{ route('profile.show', ['user' => $report->reporter->id]) }}">{{ $report->reported->fullName() }}</a>:
-                            </span>
-                            <br>
-                            <p>{{ $report->reason }}</p>
-                            <a href="{{ route('admin.reports.show', ['report' => $report->id]) }}">View full report</a>
-                            <hr>
-                        @endforeach
-                    @else
-                        <span>There are no new contact messages.</span>
-                    @endif
-                    <a href="{{ route('admin.reports.index') }}">View all reports</a>
+            <div class="row">
+                <div class="col-md-12 padding-top">
+                    <h1>Control panel</h1>
                 </div>
             </div>
-            <div class="row col-container">
-                <div class="col-md-12">
-                    @if($contact_messages->isNotEmpty())
-                        <h3>New contact messages</h3>
+            <div class="col-md-12 dashboard-content">
+                @if($reports->isNotEmpty())
+                    <h3>Reports to be reviewed</h3>
+                    <hr>
+                    @foreach($reports as $report)
+                        <span>
+                            <a href="{{ route('profile.show', ['user' => $report->reporter->id]) }}">{{ $report->reporter->fullName() }}</a>
+                            has reported
+                            <a href="{{ route('profile.show', ['user' => $report->reporter->id]) }}">{{ $report->reported->fullName() }}</a>:
+                        </span>
+                        <br>
+                        <p>{{ $report->reason }}</p>
+                        <a href="{{ route('admin.reports.show', ['report' => $report->id]) }}">View full report</a>
                         <hr>
-                        @foreach($contact_messages as $contact_message)
-                            <span><strong>From: </strong><a href="mailto:{{ $contact_message->email }}">{{ $contact_message->email }}</a></span><br>
-                            <span><strong>Message: </strong>{{ $contact_message->message }}</span><br><br>
-                            <a href="{{ route('admin.messages.show', ['contact_message' => $contact_message->id]) }}">View full message</a>
-                            <hr>
-                        @endforeach
-                    @else
-                        <span>There are no new contact messages.</span>
-                    @endif
-                    <a href="{{ route('admin.messages.index') }}">View all contact messages</a>
-                </div>
+                    @endforeach
+                @else
+                    <span>There are no new contact messages.</span>
+                @endif
+                <a href="{{ route('admin.reports.index') }}">View all reports</a>
+            </div>
+            <div class="col-md-12 dashboard-content">
+                @if($contact_messages->isNotEmpty())
+                    <h3>New contact messages</h3>
+                    <hr>
+                    @foreach($contact_messages as $contact_message)
+                        <span><strong>From: </strong><a href="mailto:{{ $contact_message->email }}">{{ $contact_message->email }}</a></span><br>
+                        <span><strong>Message: </strong>{{ $contact_message->message }}</span><br><br>
+                        <a href="{{ route('admin.messages.show', ['contact_message' => $contact_message->id]) }}">View full message</a>
+                        <hr>
+                    @endforeach
+                @else
+                    <span>There are no new contact messages.</span>
+                @endif
+                <a href="{{ route('admin.messages.index') }}">View all contact messages</a>
             </div>
         </div>
     </div>
