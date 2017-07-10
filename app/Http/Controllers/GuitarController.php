f981526f7663ae2ecc7a70934a7adea515340b14
Robin De Herdt <robindh95@gmail.com>
2017-07-10 20:13:29 +0200

    Quick fix

--- app/Http/Controllers/GuitarController.php
+++ app/Http/Controllers/GuitarController.php
@@ -274,6 +274,7 @@ class GuitarController extends Controller
                 $guitar->guitarTypes()->detach();
                 $image_amount = $guitar->guitarImages()->count();
                 $guitar->guitarImages()->delete();
+                $guitar->experiences()->delete();
                 $guitar->delete();
                 $this->subtractExp($this->user, 100 + ($image_amount * 25));
                 Session::flash('success-message', 'The guitar was deleted successfully.');
