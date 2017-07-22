09ebe1f255b4f301bd868e2bee6217c68d8eb8ca
Robin De Herdt <robindh95@gmail.com>
2017-07-22 13:35:30 +0200

    Fix voting for collection items

--- resources/views/profile/collection/show.blade.php
+++ resources/views/profile/collection/show.blade.php
@@ -105,14 +105,14 @@
                                             <div class="col-md-12">
                                                 <a href="{{ route('experience.vote', ['id' => $user->guitarExperience($guitar)->id ])}}" class="cta-button" title="Mark this experience as helpful" onclick="event.preventDefault(); vote('{{ $user->guitarExperience($guitar)->id }}', 1);">
                                                     <span>
-                                                        <i class="fa {{ Auth::check() && $user->guitarExperience($guitar)->upVotes->contains('user_id', Auth::user()->id) ? 'fa-thumbs-up' : 'fa-thumbs-o-up' }}" aria-hidden="true"></i>
-                                                        {{ $user->guitarExperience($guitar)->upVotes->count() }}
-                                                    </span>
+                                                 <i class="fa {{ Auth::check() && $user->guitarExperience($guitar)->upVotes->contains('user_id', Auth::user()->id) ? 'fa-thumbs-up' : 'fa-thumbs-o-up' }}" aria-hidden="true" id="upvote-{{ $user->guitarExperience($guitar)->id }}"></i>
+                                                  <span id="upvote-count-{{ $user->guitarExperience($guitar)->id }}">{{ $user->guitarExperience($guitar)->upVotes->count() }}</span>
+                                            </span>
                                                 </a>
                                                 <a href="{{ route('experience.vote', ['id' => $user->guitarExperience($guitar)->id ])}}" class="cta-button" title="Mark this experience as not helpful" onclick="event.preventDefault(); vote('{{ $user->guitarExperience($guitar)->id }}', 0);">
                                                     <span>
-                                                        <i class="fa {{ Auth::check() && $user->guitarExperience($guitar)->downVotes->contains('user_id', Auth::user()->id) ? 'fa-thumbs-down' : 'fa-thumbs-o-down' }}" aria-hidden="true"></i>
-                                                         {{ $user->guitarExperience($guitar)->downVotes->count() }}
+                                                        <i class="fa {{ Auth::check() && $user->guitarExperience($guitar)->downVotes->contains('user_id', Auth::user()->id) ? 'fa-thumbs-down' : 'fa-thumbs-o-down' }}" aria-hidden="true" id="downvote-{{ $user->guitarExperience($guitar)->id }}"></i>
+                                                        <span id="downvote-count-{{ $user->guitarExperience($guitar)->id }}">{{ $user->guitarExperience($guitar)->downVotes->count() }}</span>
                                                     </span>
                                                 </a>
                                                 <form action="{{ route('experience.vote', ['id' => $user->guitarExperience($guitar)->id ])}}" method="POST" id="vote-form-{{ $user->guitarExperience($guitar)->id }}">
@@ -145,10 +145,11 @@
 
 @section('scripts')
     <script>
-        function vote(id, value) {
-            $('#value-field-' +id).val(value);
-            $('#vote-form-' + id).submit();
-        }
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
 
         function showEditForm(id) {
             $('#experience-form-' + id).show();
@@ -168,6 +169,122 @@
         function closeCreateForm(id) {
             $('#experience-add-form-' + id).hide();
             $('#experience-add-link-' + id).show();
+        }
+
+        function vote(id, value) {
+            var class_list;
+
+            var selected;
+            var opposite;
+
+            var selected_vote_count;
+            var opposite_vote_count;
+
+            switch(value) {
+                case 0:
+                    selected = "downvote";
+                    opposite = "upvote";
+                    break;
+
+                case 1:
+                    selected = "upvote";
+                    opposite = "downvote";
+                    break;
+
+                default:
+                    return;
+            }
+
+            var selected_count_selector = "#" + selected + "-count-" + id;
+            var selected_icon_selector  = "#" + selected + "-" + id;
+            var opposite_count_selector = "#" + opposite + "-count-" + id;
+            var opposite_icon_selector  = "#" + opposite + "-" + id;
+
+            selected_vote_count = $(selected_count_selector).text();
+            opposite_vote_count = $(opposite_count_selector).text();
+
+            class_list  = $(selected_icon_selector).attr("class").split(' ');
+
+            for(var i = 0; i < class_list.length; i++) {
+                switch(class_list[i]) {
+                    case "fa-thumbs-o-up":
+                        if($(opposite_icon_selector).hasClass("fa-thumbs-down")) {
+                            cancelDownvote(opposite_icon_selector, id, false);
+                            opposite_vote_count--;
+                        }
+
+                        upvote(selected_icon_selector, id);
+                        selected_vote_count++;
+
+                        break;
+
+                    case "fa-thumbs-up":
+                        cancelUpvote(selected_icon_selector, id);
+                        selected_vote_count--;
+                        break;
+
+                    case "fa-thumbs-o-down":
+                        if($(opposite_icon_selector).hasClass("fa-thumbs-up")) {
+                            cancelUpvote(opposite_icon_selector, id, false);
+                            opposite_vote_count--;
+                        }
+
+                        downvote(selected_icon_selector, id);
+                        selected_vote_count++;
+
+                        break;
+
+                    case "fa-thumbs-down":
+                        cancelDownvote(selected_icon_selector, id);
+                        selected_vote_count--;
+                        break;
+                }
+            }
+
+            $(selected_count_selector).text(selected_vote_count);
+            $(opposite_count_selector).text(opposite_vote_count);
+        }
+
+        function upvote(selector, id) {
+            $(selector).removeClass("fa-thumbs-o-up").addClass("fa-thumbs-up");
+            $.post("/experience/" + id + "/vote", { value: 1 }).done(function(data) {
+                if(data == 304) {
+                    window.location = '/login';
+                }
+            });
+        }
+
+        function cancelUpvote(selector, id, send = true) {
+            $(selector).removeClass("fa-thumbs-up").addClass("fa-thumbs-o-up");
+
+            if (send) {
+                $.post("/experience/" + id + "/vote", {value: 1}).done(function(data) {
+                    if(data == 304) {
+                        window.location = '/login';
+                    }
+                });
+            }
+        }
+
+        function downvote(selector, id) {
+            $(selector).removeClass("fa-thumbs-o-down").addClass("fa-thumbs-down");
+            $.post("/experience/" + id + "/vote", {value: 0}).done(function(data) {
+                if(data == 304) {
+                    window.location = '/login';
+                }
+            });
+        }
+
+        function cancelDownvote(selector, id, send = true) {
+            $(selector).removeClass("fa-thumbs-down").addClass("fa-thumbs-o-down");
+
+            if (send) {
+                $.post("/experience/" + id + "/vote", {value: 0}).done(function(data) {
+                    if(data == 304) {
+                        window.location = '/login';
+                    }
+                });
+            }
         }
     </script>
     @include('partials.dialog-js')
